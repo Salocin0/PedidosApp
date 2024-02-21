d94134ca518f97e54dd3a58e0962a3a53ecf2829
Salocin0 <nicolasarieldurelli@gmail.com>
2024-02-21 18:53:23 -0300

    font personalizada

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import Main from './src/components/Main';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from "expo-font";
+import { fontCollection } from "./src/Styles/Fonts";
 
 export default function App() {
+  const [fontsLoaded] = useFonts(fontCollection);
+  if (!fontsLoaded) return null;
   return (
     <NavigationContainer>
       <Main/>
